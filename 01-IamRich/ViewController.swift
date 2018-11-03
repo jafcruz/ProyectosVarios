f9a8beb605bd30d6772d8adb50d2094f8fd1aace
Juan Antonio Fernandez Cruz <jafcruz@mac.com>
2018-11-03 20:41:55 +0100

    Initial Commit

---
+++ 01-IamRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-IamRich
+//
+//  Created by Juan Antonio Fernandez Cruz on 03/11/2018.
+//  Copyright © 2018 Juan Antonio Fernandez Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
